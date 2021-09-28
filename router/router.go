d3a0eecba8607191f237dc3fa3c549dc4c489886
heyujiang <1393870072@qq.com>
2021-09-28 20:02:50 +0800

    **

--- router/router.go
+++ router/router.go
@@ -8,11 +8,11 @@ import (
 	"net/http"
 )
 
-func Load(g *gin.Engine,middlewares []gin.HandlerFunc) *gin.Engine{
+func Load(g *gin.Engine, middlewares []gin.HandlerFunc) *gin.Engine {
 	g.Use(gin.Recovery())
 
 	g.NoRoute(func(ctx *gin.Context) {
-		ctx.String(http.StatusOK,"The incorrect API route.")
+		ctx.String(http.StatusOK, "The incorrect API route.")
 	})
 
 	g.Use(middleware.NoCache)
@@ -22,18 +22,20 @@ func Load(g *gin.Engine,middlewares []gin.HandlerFunc) *gin.Engine{
 
 	sdrg := g.Group("/sd")
 	{
-		sdrg.GET("/health",sd.HealthCheck)
-		sdrg.GET("/disk",sd.DiskCheck)
-		sdrg.GET("/cpu",sd.CPUCheck)
-		sdrg.GET("/ram",sd.RAMCheck)
+		sdrg.GET("/health", sd.HealthCheck)
+		sdrg.GET("/disk", sd.DiskCheck)
+		sdrg.GET("/cpu", sd.CPUCheck)
+		sdrg.GET("/ram", sd.RAMCheck)
 	}
 
-
-	userrg := g.Group("/user")
+	userrg := g.Group("/v1/user")
 	{
-		userrg.POST("/create",user.Create)
+		userrg.POST("", user.Create)       //创建用户
+		userrg.DELETE("/:id", user.Delete) //删除用户
+		userrg.PUT("/:id", user.Update)    //更新用户
+		userrg.GET("", user.List)          //用户列表
+		userrg.GET("/:id", user.Get)       //获取指定id的用户的详细信息
 	}
-
 
 	return g
 }
