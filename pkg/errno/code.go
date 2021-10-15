42fe459daafbeeb451e85adcd3dd4a95a022d4cd
heyujiang <1393870072@qq.com>
2021-10-15 19:58:25 +0800

    删除无用的错误定义

--- pkg/errno/code.go
+++ pkg/errno/code.go
@@ -5,10 +5,8 @@ var (
 	OK      = &Errno{Code: 200, Message: "OK"}
 	SUCCESS = &Errno{Code: 200, Message: "Success."}
 
-	LoginSuccess = &Errno{Code: 200, Message: "Login Success!"}
-	NoUsername   = &Errno{Code: 100, Message: "Username is no found."}
+	NoUsername = &Errno{Code: 100, Message: "Username is no found."}
 
-	ErrToken              = &Errno{Code: 20003, Message: "Error occurred while signing the JSON web token."}
 	ErrTokenInvalid       = &Errno{Code: 20004, Message: "The token was invalid."}
 	ErrMissingTokenString = &Errno{Code: 20005, Message: "The length of the `Authorization` header is zero."}
 
@@ -20,7 +18,4 @@ var (
 	ErrDeleteUser       = &Errno{Code: 20006, Message: "删除用户失败"}
 
 	ErrData = &Errno{Code: 20009, Message: "无数据"}
-
-	// user errors
-	ErrUserNotFound = &Errno{Code: 20102, Message: "The user was not found."}
 )
